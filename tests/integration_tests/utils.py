7cc9f3758a2496ffa44b0c0629a72e7878d584c4
kotolex <lex@draven.ru>
2024-02-02 10:42:25 +0500

    minor try 2 to run integrations on github

--- tests/integration_tests/utils.py
+++ tests/integration_tests/utils.py
@@ -1,11 +1,18 @@
 import random
+from pathlib import Path
 from string import ascii_lowercase
 
+from py_surreal import Surreal
 from py_surreal.clients.http_client import HttpClient
 
 URL = "http://127.0.0.1:8000/"
 
 assert HttpClient(URL).get('health').status == 200, "Start db on localhost!"
+
+file_path = Path(__file__).parent / "import.surql"
+db = Surreal(URL, 'test', 'test', ('root', 'root'), use_http=True)
+with db.connect() as connection:
+    connection.import_data(file_path)
 
 
 def get_random_series(length: int) -> str:
