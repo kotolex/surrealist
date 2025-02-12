fa80b6332781f0642bced6213fa5eee9a5a283a9
kotolex <lexman2@yandex.ru>
2025-02-12 16:49:32 +0500

    support for SDB 2.2.0
    - change ws variable

--- tests/up_db.sh
+++ tests/up_db.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-SURREAL_CAPS_ALLOW_EXPERIMENTAL=graphql surreal start memory --user root --pass root
+SURREAL_CAPS_ALLOW_EXPERIMENTAL=graphql SURREAL_WEBSOCKET_RESPONSE_BUFFER_SIZE=0 surreal start memory --user root --pass root
